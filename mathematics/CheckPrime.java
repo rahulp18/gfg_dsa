ad353a1eb1a07e8dcc3bd7d3ad9f03ec1f3caa9f
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-06-08 14:06:21 +0530

    *

---
+++ mathematics/CheckPrime.java
@@ -0,0 +1,34 @@
+package mathematics;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        System.out.println(isPrimeBasic(12));
+        System.out.println(isPrimeEffeicentWay(11));
+        System.out.println(moreEfficentWay(11));
+    }
+    public static boolean isPrimeBasic(int n){
+        if(n==1) return false;
+
+        for(int i=2;i<n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public static boolean isPrimeEffeicentWay(int n){
+        if(n==1) return false;
+
+        for(int i=2;i*i<n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public static boolean moreEfficentWay(int n){
+        if(n==1) return false;
+        if(n ==2 || n ==3) return true;
+  if(n%2==0 || n%3==0) return false;
+        for(int i=5;i*i<=n;i=i+6){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
