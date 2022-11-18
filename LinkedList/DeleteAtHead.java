bf64182838317c0e9ba3d20e2eb818f766f4ec6e
Rahul Pradhan <pradhanrahul3346@gmail.com>
2022-11-18 12:05:44 +0530

    simple

---
+++ LinkedList/DeleteAtHead.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class DeleteAtHead {
+    public static void main(String[] args) {
+        Node head = null;
+        head = atEnd(200, head);
+        head = atEnd(285, head);
+        head = atEnd(892, head);
+        head = atEnd(652, head);
+        head = atEnd(202, head);
+        head = deleteFromTail(head);
+
+        Node curr = head;
+        while (curr.next != null) {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+        System.out.println(curr.data);
+    }
+
+    public static Node deleteNode(Node head) {
+        if (head == null) {
+            return head;
+        }
+        head = head.next;
+        return head;
+    }
+
+    public static Node deleteFromTail(Node head) {
+
+        if (head == null) {
+            return head;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+
+        }
+        temp.next = null;
+
+        return head;
+    }
+
+    public static Node atEnd(int data, Node head) {
+        Node new_node = new Node(data);
+        if (head == null) {
+            head = new_node;
+            return head;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = new_node;
+
+        return head;
+    }
+
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
---
+++ LinkedList/InsertNodeAtHead.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class InsertNodeAtHead {
+
+    public static void main(String[] args) {
+
+        Node head = null;
+        head = insertAtHead(0, head);
+        head = insertAtHead(50, head);
+        head = insertAtHead(580, head);
+        head = insertAtHead(90, head);
+        printlist(head);
+    }
+
+    public static void printlist(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+
+    public static Node insertAtHead(int data, Node head) {
+        Node new_node = new Node(data);
+
+        new_node.next = head;
+
+        return new_node;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+
+        this.data = data;
+        this.next = null;
+    }
+
+}
